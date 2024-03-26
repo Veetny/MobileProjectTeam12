f3b3c2d446f8d9f1189f6c8c454f911c8ae1c4a4
Veetny <117356851+Veetny@users.noreply.github.com>
2024-03-26 15:02:30 +0200

    d

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 
 export default function App() {
