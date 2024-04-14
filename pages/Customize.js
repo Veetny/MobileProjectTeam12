970fd6a93ca212d8d105b9d01231bb68fe6d539b
Tompetin <tommisalo.jokeri@elisanet.fi>
2024-04-14 22:02:52 +0300

    page changes

--- pages/Customize.js
+++ pages/Customize.js
@@ -2,8 +2,8 @@ import {useState } from "react";
 import { View, Text } from "react-native";
 import { Icon } from 'react-native-paper';
 
-//I didn't manage to get this to do really anything unfortunately (managed to escalate everything so I just decided to have only the radio button :C)
-export default function Settings() {
+
+export default function Customize() {
 
 
     return (
---
+++ pages/Forecast.js
@@ -0,0 +1,15 @@
+import {useState } from "react";
+import { View, Text } from "react-native";
+import { Icon } from 'react-native-paper';
+
+
+export default function Forecast() {
+
+
+    return (
+        <View>
+            <Text>Forecast</Text>
+        </View>
+
+    );
+}
