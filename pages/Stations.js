623b6c0835846e7ed9b23174a5cb55f5605a99e3
oggysi <oskari.ontero@gmail.com>
2024-04-21 18:47:42 +0300

    trendikäs

---
+++ pages/Stations.js
@@ -0,0 +1,15 @@
+import {useState } from "react";
+import { View, Text } from "react-native";
+import { Icon } from 'react-native-paper';
+
+
+export default function Stations() {
+
+
+    return (
+        <View>
+            <Text>Stations</Text>
+        </View>
+
+    );
+}
