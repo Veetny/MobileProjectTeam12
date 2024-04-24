9c365e87ad932c977cc65f038bb12204ccab29b3
Veetny <117356851+Veetny@users.noreply.github.com>
2024-04-24 18:48:37 +0300

    Man hitting griddy very mui importante!!?!(Gone wrong)!!!! Very bad and not good.

--- pages/Weather.js
+++ pages/Weather.js
@@ -1,15 +1,55 @@
-import {useState } from "react";
-import { View, Text } from "react-native";
-import { Icon } from 'react-native-paper';
+import React, { useRef, useEffect } from 'react';
+import { View, Image, StyleSheet, Animated, Easing } from 'react-native';
 
+export default function GriddyAnimation() {
+    const translateX = useRef(new Animated.Value(0)).current;
 
-export default function Weather() {
+    useEffect(() => {
+        const moveMan = () => {
+            Animated.loop(
+                Animated.sequence([
+                    Animated.timing(translateX, {
+                        toValue: 150,
+                        duration: 1000,
+                        easing: Easing.linear,
+                        useNativeDriver: true,
+                    }),
+                    Animated.timing(translateX, {
+                        toValue: -150,
+                        duration: 1000,
+                        easing: Easing.linear,
+                        useNativeDriver: true,
+                    }),
+                ]),
+                {
+                    iterations: 2 // change iterations as you need
+                }
+            ).start();
+        };
 
+        moveMan();
+    }, [translateX]);
 
     return (
-        <View>
-            <Text>Weather</Text>
+        <View style={styles.container}>
+            <Animated.Image
+                source={require('./man.png')}
+                style={[styles.man, { transform: [{ translateX }] }]}
+                resizeMode="contain"
+            />
         </View>
-
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    man: {
+        width: 100,
+        height: 100,
+    },
+});
