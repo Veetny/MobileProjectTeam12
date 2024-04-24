0c6a14aa978fdc23d65ea58dde0695850c2624a2
Veetny <117356851+Veetny@users.noreply.github.com>
2024-04-24 18:27:35 +0300

    Kymmenia customizeja

--- pages/Customize.js
+++ pages/Customize.js
@@ -1,15 +1,38 @@
-import {useState } from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
-import { Icon } from 'react-native-paper';
-
+import { RadioButton } from 'react-native-paper';
+import style from "../styles/style";
 
 export default function Customize() {
-
+    const [checked, setChecked] = useState('first');
 
     return (
-        <View>
+        <View style={style.KymmeniaPaddingeja}>
             <Text>Customize</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <RadioButton
+                    value="first"
+                    status={ checked === 'first' ? 'checked' : 'unchecked' }
+                    onPress={() => setChecked('first')}
+                />
+                <Text>Default</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <RadioButton
+                    value="second"
+                    status={ checked === 'second' ? 'checked' : 'unchecked' }
+                    onPress={() => setChecked('second')}
+                />
+                <Text>Light mode</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <RadioButton
+                    value="third"
+                    status={ checked === 'third' ? 'checked' : 'unchecked' }
+                    onPress={() => setChecked('third')}
+                />
+                <Text>Dark mode</Text>
+            </View>
         </View>
-
     );
 }
